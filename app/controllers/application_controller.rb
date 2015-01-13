31fa4caa14fef9b2ae234accca2a2b311ee27282
Grant Alexander <grant.fricano@gmail.com>
2015-01-13 15:35:52 +0000

    tool bar and logo

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,22 +2,16 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-  before_filter :load_tweets
 
+  @tweets = []
   client = Twitter::REST::Client.new do |config|
     config.consumer_key = 'n6yB5IhmDxAzn93AOIcJhcYS4'
     config.consumer_secret = 'GfWCpo8uWWfGc0e6NrB23gOqdB43ZbSZObaa1eS17dZ2uwJkkn'
-    config.oauth_token = '113056053-IRtNOxv3CKA7Cb30KTTZ3KZWUon8dZZtmRsYclHR'
-    config.oauth_token_secret = 'OZd49wt3EpTH8v3DhKFT2WMiK9X6BMLUKvXnHdCwi0bYs'
+    config.access_token = '113056053-IRtNOxv3CKA7Cb30KTTZ3KZWUon8dZZtmRsYclHR'
+    config.access_token_secret = 'OZd49wt3EpTH8v3DhKFT2WMiK9X6BMLUKvXnHdCwi0bYs'
   end
 
-  def client.get_all_tweets(user)
-    options = {:count => 3, :include_rts => true}
-      user_timeline(user, options)
-  end
-
-  #@tweets = Twitter.user_timeline('ale_la_grande', {count: 3})
-
-  def load_tweets
+  client.search("to:justinbieber marry me", result_type: "recent").take(3).each do |tweet|
+    @tweets << tweet
   end
 end
